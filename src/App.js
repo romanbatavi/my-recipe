d1bdf74bcd190aa43304715427510c2e14594296
romanbatavi <romanbatavi98@gmail.com>
2021-04-21 19:24:55 +0700

    create Recipe.js, props app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, {useEffect, useState} from "react";
-// import logo from './logo.svg';
 import './App.css';
+import Recipe from "./Recipe"
 
 const App = () => {
   const APP_ID = '669c50ff';
   const APP_KEY = 'a48f6771b754b5fa85f57ee5fde64531';
 
-  useEffect (() => {
+  const [recipes, setRecipes] = useState([]);
 
+  useEffect (() => {
+    getRecipes();
   }, []);
 
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const data = response.json();
-    console.log(data);
-  }
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
 
   return (
     <div className="App">
@@ -22,7 +25,12 @@ const App = () => {
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick ={() => setCounter(counter + 1)}>{counter}</h1>
+      {recipes.map(recipe => (
+        <Recipe title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image} 
+        />
+      ))}
     </div>
   );
 };
