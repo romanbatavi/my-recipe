be9eb1a8d3bd96e4bfac01083640c696153fe658
romanbatavi <romanbatavi98@gmail.com>
2021-04-21 18:51:25 +0700

    response

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 // import logo from './logo.svg';
 import './App.css';
 
@@ -6,14 +6,23 @@ const App = () => {
   const APP_ID = '669c50ff';
   const APP_KEY = 'a48f6771b754b5fa85f57ee5fde64531';
 
-  const exapleReq = 
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+  useEffect (() => {
+
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json();
+    console.log(data);
+  }
+
   return (
-    <div className="APP">
+    <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
         <button className="search-button" type="submit">Search</button>
       </form>
+      <h1 onClick ={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 };
