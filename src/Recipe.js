0418e7111bc219b03d07b0a47b0fe57ec85c14b2
romanbatavi <romanbatavi98@gmail.com>
2021-04-21 20:16:32 +0700

    fix img

--- src/Recipe.js
+++ src/Recipe.js
@@ -11,7 +11,7 @@ const Recipe = ({title, calories, image, ingredients}) => {
                 ))}    
             </ol>   
             <ol>{calories}</ol>
-            <img src={image} alt=""/>
+            <img className={style.image} src={image} alt=""/>
         </div>
     );
 };
